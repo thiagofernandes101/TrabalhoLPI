02dcfe2b4c6057e4fb2a3679535d1a420811e666
MarceloMax50 <maxmelo93@gmail.com>
2019-11-24 19:43:06 -0300

    alterações

---
+++ YouPlay2019/YouPlay2019/wwwroot/js/scripts.js
@@ -0,0 +1,7 @@
+﻿function verImagem() {
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL(document.getElementById("Imagem").files[0]);
+    oFReader.onload = function (oFREvent) {
+        document.getElementById("fotoPessoa").src = oFREvent.target.result;
+    };
+}
